d166c272678fe8b12a1d22ed855bf1c655d49dc1
Ravi Shanakar Gupta <159308710+Codewithravi-7@users.noreply.github.com>
2025-11-02 19:00:09 +0530

    Update Find Pivot Index.cpp

--- Array II/Find Pivot Index.cpp
+++ Array II/Find Pivot Index.cpp
@@ -1,1 +1,26 @@
+// https://leetcode.com/problems/find-pivot-index/description/ Leetcode (724)
+// Time Complexity :- O(n) && Space Complexity :- O(1)
 
+class Solution {
+public:
+    int pivotIndex(vector<int>& nums) {
+        int n = nums.size();
+
+        int sum = 0;
+        for(int &x : nums){
+            sum += x;
+        }
+        int cs = 0;
+
+        for(int i=0; i<n; i++){
+            int ls = cs;
+            int rs = sum - cs - nums[i];
+
+            if(ls == rs) return i;
+
+            cs += nums[i];
+        }
+
+        return -1;
+    }
+};
