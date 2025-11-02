edfa1936f7d6e5919322b9527c37bd422fde89fb
Ravi Shanakar Gupta <159308710+Codewithravi-7@users.noreply.github.com>
2025-11-02 18:57:09 +0530

    Fix removeDuplicates function logic
    Fix the logic in the removeDuplicates function to correctly count unique elements.

--- Array II/Remove Duplicates from Sorted Array.cpp
+++ Array II/Remove Duplicates from Sorted Array.cpp
@@ -1,1 +1,26 @@
+// https://leetcode.com/problems/remove-duplicates-from-sorted-array/
+// Time Complexity :- O(n) && Space Complexity :- O(1)
 
+
+
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        // Size of Array
+        int n =nums.size();
+        if(n == 0) return 0;
+
+        // Using Two Pointer
+
+        int i=0;
+
+        // if nums[j] != nums[i] that means We Found a new unique Element
+        for(int j=1; j<n; j++){
+            if(nums[j] != nums[i]){
+                i++;
+                nums[i] = nums[j];
+            }
+        }
+        return i+1;
+    }
+};
