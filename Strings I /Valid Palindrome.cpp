d00df7d5341bf3dca70ec2be258bdc1cd80c8257
Ravi Shanakar Gupta <159308710+Codewithravi-7@users.noreply.github.com>
2025-11-03 01:30:35 +0530

    Clarify time and space complexity in Valid Palindrome
    Updated time and space complexity comments for clarity.

--- Strings I /Valid Palindrome.cpp
+++ Strings I /Valid Palindrome.cpp
@@ -1,3 +1,27 @@
 // Question Link :- https://leetcode.com/problems/valid-palindrome/description/ Leetcode(125)
-// Time COmplexity :- 
-// Space Complexity :- 
+// Time COmplexity :- O(n)
+// Space Complexity :- O(n)
+
+
+class Solution {
+public:
+    bool isPalindrome(string s) {
+      // create a string clean to store all the lowercase letter
+        string clean = "";
+        for(char ch : s){
+          // Check if character is a alphanumeric then convert into the lowercase and add in clean string 
+            if(isalnum(ch)){
+                clean += tolower(ch);
+            }
+        }
+        int left = 0;
+        int right = clean.length() - 1;
+
+        while(left < right){
+            if(clean[left] != clean[right]) return false;
+            left++;
+            right--;
+        }
+        return true;
+    }
+};
