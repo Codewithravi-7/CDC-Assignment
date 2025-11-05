8349b333977947ed67f0e1dfd4c965a1b437e794
Ravi Shanakar Gupta <159308710+Codewithravi-7@users.noreply.github.com>
2025-11-05 18:57:46 +0530

    Add Solution class for finding pivot index
    Implement pivot index calculation in a new class.

---
+++ Recursion II /Find Pivot Index.cpp
@@ -0,0 +1,28 @@
+// Question Link :- https://leetcode.com/problems/find-pivot-index/description/ Leetcode (724)
+// Time Complexity:- O(n)
+// SPace Complexity :- O(1)
+
+
+class Solution {
+public:
+    int pivotIndex(vector<int>& nums) {
+        int n = nums.size();
+
+        int sum = 0;
+        for(int &x : nums){
+            sum += x;
+        }
+        int cs = 0;
+
+        for(int i=0; i<n; i++){
+            int ls = cs;
+            int rs = sum - cs - nums[i];
+
+            if(ls == rs) return i;
+
+            cs += nums[i];
+        }
+
+        return -1;
+    }
+};
